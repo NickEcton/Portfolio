888c1d07b03f3f62ce3df191ed7155d4715e0475
Nick Ecton <ecton.nick@gmail.com>
2020-01-17 10:59:44 -0800

    build header and first content

--- assets/js/linked.js
+++ assets/js/linked.js
@@ -1,19 +1,41 @@
 let i = document.querySelector('#individual');
 let en = document.querySelector('#enterprise');
 
+let enDefaultOpac = 0.5;
+let iDefaultOpac = 1.0;
+
 i.addEventListener('click', (e) => {
-    i.style.borderBottom = '2px solid blue';
-    en.style.borderBottom = 'none';
+    i.style.opacity = '1.0';
+    en.style.opacity = '0.5';
     document.querySelector('#individualContainer').style.display = '';
     document.querySelector('#enterpriseContainer').style.display = 'none'
+    i.classList.add('selected')
+    en.classList.remove('selected');
 })
 
 en.addEventListener('click', (e) => {
-    en.style.borderBottom = '2px solid blue';
-    i.style.borderBottom = 'none';
+    en.style.opacity = '1.0';
+    i.style.opacity = '0.5';
     document.querySelector('#individualContainer').style.display = 'none';
     document.querySelector('#enterpriseContainer').style.display = ''
+    en.classList.add('selected')
+    i.classList.remove('selected');
 })
+
+let items = [i, en]
+
+for(let i = 0; i < items.length; i++) {
+    items[i].addEventListener('mouseover', () => {
+        items[i].style.opacity = 1.0;
+    })
+    items[i].addEventListener('mouseleave', () => {
+        if(items[i].classList.contains('selected')) {
+            items[i].style.opacity = 1.0;
+        } else {
+            items[i].style.opacity = 0.5;
+        }
+    })
+}
 
 let jonahText = '"Being a student full-time, I was interested in finding remote contract work that could help pay for my schooling. linked allowed me to continue to network even while I was in class or studying. Thanks to linked, I was able to find a part-time position that allows me to focus on schooling, while also earning money to help pay for it." \n -Jonah'
 let reaganText ='"As a saleswoman for a savvy tech startup, I need to get in contact with as many business leaders as possible on a daily basis. While cold-calling and networking has its advantages, the passive power that linked gives me to expand my presence on LinkedIn is paramount. With linked, clients actually come to me. Giving me a huge advantage over my competitors." \n -Reagan'
