adb206b3e387b4d038d1e86e7e4e344431110fcf
Nick Ecton <ecton.nick@gmail.com>
2020-01-20 19:30:11 -0800

    increase functionality

--- assets/js/linked.js
+++ assets/js/linked.js
@@ -41,24 +41,33 @@ let jonahText = '"Being a student full-time, I was interested in finding remote 
 let reaganText ='"As a saleswoman for a savvy tech startup, I need to get in contact with as many business leaders as possible on a daily basis. While cold-calling and networking has its advantages, the passive power that linked gives me to expand my presence on LinkedIn is paramount. With linked, clients actually come to me. Giving me a huge advantage over my competitors." \n -Reagan'
 let kristianText = ' "I have a passion for the outdoors and for technology, but I found it difficult to connect with like-minded individuals who were interested in making a difference. The power of linked comes from its ability to work even when I can\'t. While i\'m out on the mountain or wading  down stream, linked is hard at work establishing and maintaining my connections online." \n -Kristian'
 
+let jonahPhoto = 'file:///Users/nickecton/Desktop/Portfolio/images/jonahFace.jpeg'
+let reaganPhoto = 'file:///Users/nickecton/Desktop/Portfolio/images/reaganFace.jpeg'
+let kristianPhoto = 'file:///Users/nickecton/Desktop/Portfolio/images/kristianFace.jpeg'
+
 let images = document.querySelectorAll('.face');
 
 let clickedFace = function(e) {
+    // debugger
     let text = document.querySelector('#testimonialText');
     images = document.querySelectorAll('.face');
     let target = e.target.id;
-    for(let i = 0; i < images.length; i++) {
-        images[i].style.boxShadow = 'none'
-        if(images[i].id === target) {
-            images[i].style.boxShadow = "cyan 0px 0px 3pt 2pt"
-        }
-    }
-    if(target === 'kristian') {
+
+    if(e.target.src === kristianPhoto) {
         text.innerText = kristianText;
-    } else if(target === 'jonah') {
+    } else if(e.target.src === jonahPhoto) {
         text.innerText = jonahText;
-    } else if(target == 'reagan') {
+    } else if(e.target.src == reaganPhoto) {
         text.innerText = reaganText;
+    }
+
+    if(target !== 'foreground') {
+        let foreground = document.querySelector('#foreground')
+        let foregroundSrc = foreground.src;
+        let targetSrc = e.target.src;
+
+        foreground.src = targetSrc;
+        e.target.src = foregroundSrc;
     }
 
 }
